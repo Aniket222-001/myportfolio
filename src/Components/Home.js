b24bcce0d563eea07e39a52a3627db20da1ab7f4
Aniket222-001 <139674804+Aniket222-001@users.noreply.github.com>
2024-02-23 18:59:48 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './Home.css'
+import { Typewriter } from 'react-simple-typewriter'
 
 const Home = () => {
   return (
     <div className='Container' id='home'>
       <div className='box1'>
-        <h1>Hi, I'm Aniket</h1>
-        <p className='txt2'>I'm a full stack devloper with lots of experience in React and Node js.
-            <br />
-            React out to me to 
-            learn more and hire me
+        <h1>
+        <Typewriter words={[`Hi , I'm Aniket`]} delaySpeed={5000}>
+        </Typewriter>   
+        </h1>
+        <p className='txt2'><span><Typewriter words={[`I m full stack devloper with lots of experience in React and Node js.`
+        ,'React out to me to  learn more and hire me']}
+        delaySpeed={1000}>
+        </Typewriter>
+        </span>        
         </p>
         <button><a  className='btn1' href="https://drive.google.com/file/d/1G5_w-7g_CarlY1Or8Zk-KfOACvPVeVTJ/view?usp=drive_link" 
         target="_blank" >
